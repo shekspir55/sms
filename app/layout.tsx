399423a96a93146c10a4da7789a62dfb334f24c8
Ruben Shekspir <shekspir55@gmail.com>
2023-07-07 15:20:42 +0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,48 @@
-import './globals.css'
+import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Providers from "./providers";
 
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'GetSMS: Allow your business go international.',
-  description: 'Receive SMS from anywhere from anywhere in the world: Reserve Phone Numbers in 20+ Countries, Including US 🇺🇸, UK 🇬🇧, and EU 🇪🇺, and Receive OTP and SMS Within Our Application with a flat monthly fee.',
-}
+  title: "GetSMS: Allow your business go international.",
+  description:
+    "Receive SMS from anywhere from anywhere in the world: Reserve Phone Numbers in 20+ Countries, Including US 🇺🇸, UK 🇬🇧, and EU 🇪🇺. Receive SMS/OTP with a simple flat monthly fee.",
+};
+
+const googleID = process.env.GOOGLE_TAG;
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
+      <Script
+        strategy="afterInteractive"
+        src={"https://www.googletagmanager.com/gtag/js?id=" + googleID}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${googleID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { metadata } from "./layout";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center p-1 font-mono bg-gray-300 bg-pattern">
+      <main className="flex min-h-screen flex-col items-center justify-center p-1 font-mono bg-gray-300 background-pattern">
         <div className="">
           <div className="container">
             <div className="row">
@@ -12,19 +12,20 @@ export default function Home() {
                   <b>{metadata.title}</b>
                 </h1>
                 <h2 className="h3 mt-4">{metadata.description}</h2>
-                <h3 className="h1 mt-4 font-bold">
-                  Special deal{" "}
+                <h3 className="h1 mt-4">
                   <i>
                     <s>20$</s>
                   </i>{" "}
-                  10$
+                  <b>10$</b>
+                  <span className="h5"> Special offer </span>
                 </h3>
-                <button className="mt-4 middle none center rounded-lg bg-blue-500 py-3.5 px-7 font-sans text-sm font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none text-xl">
+                <a className="mt-4 middle none text-center rounded-lg bg-blue-500 py-3.5 px-7 font-sans text-sm font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none text-xl" 
+                href="#">
                   REGISTER
-                </button>
+                </a>
               </div>
               <div className=" mt-4 col-md d-none d-lg-block flex flex-col">
-                <img className="floating" src="/telegram.png" alt="" />
+                <img className="floating" src="/telegram.png" alt="telegram logo" />
               </div>
             </div>
           </div>
