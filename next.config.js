3663c94ffb18919b7b9c4285bb04ef6b5aa2bbbc
Ruben Shekspir <shekspir55@gmail.com>
2023-07-10 14:46:05 +0400

    build fixed

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    async rewrites() {
-      return [
-        {
-          source: '/mail-subscribe',
-          destination: 'https://gmail.us17.list-manage.com/subscribe/post?u=93a242fec4477bfb15bfdef9e&amp;id=ae65beb7a2&amp;f_id=003960e0f0',
-        },
-      ]
-    },
+    output: 'export',
   }
    
 module.exports = nextConfig
